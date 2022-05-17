0738b1cd5c525f1455eee112b7ba7c3145987efb
crisgarlez <escipion25@gmail.com>
2022-05-16 19:04:08 -0500

    Add icons

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { BsXLg } from "react-icons/bs";
 
 function Box(props) {
   return(
-    <div className='w-full h-full border-x border-x-gray-400'>
-      <button className={`${props.color} w-full h-full border-0`} onClick={() => {props.toggleBox(props.parentId, props.id)}}>
-        {(props.boxValue) ? <span>X</span> : <span>&nbsp;</span>}
+    <div className={`${props.color} w-full h-full border-x border-x-gray-400`}>
+      <button className={`w-full h-full border-0 flex justify-center items-center`} onClick={() => {props.toggleBox(props.parentId, props.id)}}>
+        {(props.boxValue) ? <BsXLg className='text-gray-600 my-1'/> : <span>&nbsp;</span>}
       </button>
     </div>
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsArrowRepeat } from "react-icons/bs";
 
 function Header({reset}) {
   return(
@@ -8,10 +9,10 @@ function Header({reset}) {
       </div>
       <div>
         <button
-          className='inline-block text-sm px-4 py-2 leading-none border rounded text-gray-700 border-gray-700 lg:mt-0'
+          className='inline-block text-lg px-4 py-2 leading-none border rounded text-gray-700 border-gray-700 lg:mt-0'
           onClick={() => reset()}
         >
-          Reiniciar
+          <BsArrowRepeat/>
         </button>
       </div>
     </header>
